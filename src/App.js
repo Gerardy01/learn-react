42591d99ce73cb4ec627a3bbb136e785b9d52682
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2022-04-23 14:00:16 +0700

    app setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import { useState } from "react";
 import "./App.css"
-import Todo from "./components/todo/todo.js";
 
 
 function App() {
-
-  const [getValue, setValue] = useState("");
-
-  const ubah = () => {
-    if (!getValue || getValue == "ini adalah todo list") {
-        setValue("this is todo list")
-    } else {
-        setValue("ini adalah todo list")
-    }
-  }
-
   return (
-    <div className="App">
-      <h1>My Todo List</h1>
-      <Todo theValue={getValue} />
-      <button onClick={ubah}>ubah</button>
-    </div>
+    <>
+    <h1>Hello, World</h1>
+    </>
   )
 }
 
